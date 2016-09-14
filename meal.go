ea63d5d194d6077fb3e049b10d930b98e47f07da
Kilian Koeltzsch <me@kilian.io>
2016-09-15 01:34:15 +0200

    inline meal's price struct

--- meal.go
+++ meal.go
@@ -8,24 +8,20 @@ import (
 
 // A Meal object
 type Meal struct {
-	ID       int         `json:"id"`
-	Name     string      `json:"name"`
-	Category string      `json:"category"`
-	Prices   *MealPrices `json:"prices"`
-	Notes    []string    `json:"notes"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Category string `json:"category"`
+	Prices   struct {
+		Students  *float32 `json:"students"`
+		Employees *float32 `json:"employees"`
+		Pupils    *float32 `json:"pupils"`
+		Others    *float32 `json:"others"`
+	} `json:"prices"`
+	Notes []string `json:"notes"`
 }
 
 func (m Meal) String() string {
 	return m.Name
-}
-
-// A MealPrices object
-// Caveat: Any of the possible prices can be nil pointers.
-type MealPrices struct {
-	Students  *float32 `json:"students"`
-	Employees *float32 `json:"employees"`
-	Pupils    *float32 `json:"pupils"`
-	Others    *float32 `json:"others"`
 }
 
 // TodaysMeals is a helper wrapper for Meals() for today's menu
